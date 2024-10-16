02dc174b91bae4f07bc76bc0f39f09d4844d11f4
Nathan Shturm <exterminathan@gmail.com>
2024-10-15 22:01:32 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 import "./style.css";
 
-const APP_NAME = "Hola";
+const APP_NAME = "Canvas";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+//Title
+const titleEL = document.createElement("h1");
+titleEL.textContent = APP_NAME;
+
+app.append(titleEL);
+
+
+
+//Main Canvas
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+
+app.append(canvas);
